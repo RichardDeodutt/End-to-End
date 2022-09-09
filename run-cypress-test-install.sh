2bb10b2f55c7b0485f14d987ddcea6c83ac8ba32
Richard Deodutt <richarddeodutt@gmail.com>
2022-09-09 01:07:53 +0000

    Edit the file manually

--- run-cypress-test-install.sh
+++ run-cypress-test-install.sh
@@ -2,20 +2,12 @@
 
 #This script is to run the cypress test
 
-TestFile='cypress/integration/test.spec'
-
-IPandPort='127.0.0.1:5000'
+TestFile='./cypress/integration/test.spec'
 
 Log="Cypress-Run.log"
 
-#Replace IP and Port
-cd kura_test_repo && cat "$TestFile" | sed "s/127.0.0.1:5000/$IPandPort/g" > $TestFile && cd - && echo "Replaced IP and Port" >> $Log
-
-#Replace the Title
-cd kura_test_repo && cat "$TestFile" | sed "s/URL Shortener/url shortener/g" > $TestFile && cd - && echo "Replace Title" >> $Log
-
 #Run the test
-cd kura_test_repo && npx cypress run –spec $TestFile && cd - && echo "Ran Test" >> $Log
+cd kura_test_repo && npx cypress run --spec "$TestFile" && cd .. && echo "Ran Test" >> $Log
 
 #successful
 echo "Run successful" >> $Log
