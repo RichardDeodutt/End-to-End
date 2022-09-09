9f979b8cac53e838ce2ce9a43ec1cd5a6e4b3b4d
Richard Deodutt <richarddeodutt@gmail.com>
2022-09-09 00:07:19 +0000

    Run cypress test script

---
+++ run-cypress-test-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#This script is to run the cypress test
+
+TestFile='./cypress/integration/test.spec'
+
+IPandPort='127.0.0.1:5000'
+
+Log="Cypress-Run.log"
+
+#Replace IP and Port
+cat "$TestFile" | sed "s/127.0.0.1:5000/$IPandPort/g" > $TestFile && echo "Replaced IP and Port" >> $Log
+
+#Replace the Title
+cat "$TestFile" | sed "s/URL Shortener/url shortener/g" > $TestFile && echo "Replace Title" >> $Log
+
+#Run the test
+npx cypress run –spec $TestFile  && echo "Ran Test" >> $Log
+
+#successful
+echo "Run successful" >> $Log
+exit 0
