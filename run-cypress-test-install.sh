e5322e811c30985960f5bad7a7d0370b0b986f17
Richard Deodutt <richarddeodutt@gmail.com>
2022-09-09 00:17:23 +0000

    Fixed pathing issues

--- run-cypress-test-install.sh
+++ run-cypress-test-install.sh
@@ -2,20 +2,20 @@
 
 #This script is to run the cypress test
 
-TestFile='kura_test_repo/cypress/integration/test.spec'
+TestFile='cypress/integration/test.spec'
 
 IPandPort='127.0.0.1:5000'
 
 Log="Cypress-Run.log"
 
 #Replace IP and Port
-cat "$TestFile" | sed "s/127.0.0.1:5000/$IPandPort/g" > $TestFile && echo "Replaced IP and Port" >> $Log
+cd kura_test_repo && cat "$TestFile" | sed "s/127.0.0.1:5000/$IPandPort/g" > $TestFile && cd - && echo "Replaced IP and Port" >> $Log
 
 #Replace the Title
-cat "$TestFile" | sed "s/URL Shortener/url shortener/g" > $TestFile && echo "Replace Title" >> $Log
+cd kura_test_repo && cat "$TestFile" | sed "s/URL Shortener/url shortener/g" > $TestFile && cd - && echo "Replace Title" >> $Log
 
 #Run the test
-npx cypress run –spec $TestFile  && echo "Ran Test" >> $Log
+cd kura_test_repo && npx cypress run –spec $TestFile && cd - && echo "Ran Test" >> $Log
 
 #successful
 echo "Run successful" >> $Log
